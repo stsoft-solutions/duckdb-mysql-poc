f8d9f9eca76e47e955eeeee27cb0c8600c2b73ae
Sergey Smirnov <s.smirnov@robopro.team>
2026-02-20 15:05:18 +0200

    Initial TypeScript exporter project setup
    Added new TypeScript/Node.js "exporter" project with strict config, ESLint, and debugging support. Updated solution structure, added sample config, and included all boilerplate files for development and build. Replaced and corrected `default.json5` configuration.

--- exporter/app.js
+++ exporter/app.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('Hello world');
+import config from 'config';
+import Load from 'config/util.js';
+
+config.util.setModuleDefaults('source_db', {
+    host: 'localhost',
+    port: 5432,
+    database: 'mydb',
+    user: 'admin'
+});
+Load.
+/**
+ * @typedef {Object} SourceDbOptions
+ * @property {string} host
+ * @property {number} port
+ * @property {string} database
+ * @property {string} user
+ * @property {string} [password]
+ */
+
+/**
+ * @type {SourceDbOptions}
+ */
+const dbOptions = config.get('source_db');
+
+
+console.log(dbOptions.database);
+console.log(dbOptions.user);
+
